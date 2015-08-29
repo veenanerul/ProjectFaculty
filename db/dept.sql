df9c2ca97b5eb6a470f5ed749dcab526d5a800a5
Komal <komalssingh96@gmail.com>
2015-08-29 06:24:01 +0530

    dept database added

---
+++ db/dept.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS ProjectFaculty;
+USE ProjectFaculty;
+CREATE TABLE IF NOT EXITS dept(
+	id INT(11) UNSIGNED AUTO_INCREMENT NOT NULL,
+	name VARCHAR(30) ,
+	description TEXT,
+	primary key(id)
+);
+INSERT INTO dept(name,description) VALUES( 'Kavitha S','PROFESSOR');
