d81487eeaa35a12a9175e5368de07159310a3986
Mriyam Tamuli <mbtamuli@gmail.com>
2015-09-01 14:12:58 +0000

    Corrected mistakes in all the files for database

--- db/teachers.sql
+++ db/teachers.sql
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS teachers(
 	last_login_ip int unsigned,
 	primary key(id)
 );
-INSERT INTO teachers (email,password,last_login_date,last_login_ip) values("snehkore@gmail.com","ww12345",20150512,INET_ATON("123.456.789.289"));
-INSERT INTO teachers (email,password,last_login_date,last_login_ip) values("minalb@gmail.com","ww12tt",20150611,INET_ATON("121.453.789.799"));
+INSERT INTO teachers (email,password,last_login_date,last_login_ip) values("snehkore@gmail.com","ww12345",20150512,INET_ATON("10.0.0.10"));
+INSERT INTO teachers (email,password,last_login_date,last_login_ip) values("minalb@gmail.com","ww12tt",20150611,INET_ATON("10.0.0.11"));
 
--- db/user.sql
+++ db/user.sql
@@ -1,13 +1,13 @@
 CREATE DATABASE IF NOT EXISTS ProjectFaculty;
 USE ProjectFaculty;
 CREATE TABLE IF NOT EXISTS users(
-  id INT UNSIGNED AUTO_INCREMENT NOT NULL,
-  TeacherID INT,
+  id INT(11) UNSIGNED AUTO_INCREMENT NOT NULL,
+  TeacherID INT(11) UNSIGNED,
   level BOOLEAN DEFAULT 1,
   status BOOLEAN DEFAULT 0,
   PRIMARY KEY (id),
   FOREIGN KEY (TeacherID) REFERENCES teachers(id)
-};
+);
 
 INSERT INTO users(TeacherID, level, status) VALUES(1, 1, 0);
 INSERT INTO users(TeacherID, level, status) VALUES(1, 1, 0);
